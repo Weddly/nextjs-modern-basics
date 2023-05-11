79a7f6c77230fece30cf2da6e68067b59e9b6649
Weddly <weddly@hotmail.com>
2023-05-10 21:26:48 -0300

    TWD #1: TimeUpdate Static Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-black text-white text-4xl">
       <Icon24Hours size={100} strokeWidth={0.5} />
-      <span>Fundamentos</span>
+      <span>Fundaments</span>
     </div>
   )
 }
